64e213a31c62ee0229d5869209b107461fd8f7bd
Ho Thien Lac <hothienlac@gmail.com>
2025-04-19 17:43:02 +0700

    Refactor weather API request to use URL encoding for parameters

--- backend/app/api/weather/services.py
+++ backend/app/api/weather/services.py
@@ -1,20 +1,17 @@
 import requests
 from loguru import logger
 from app.configs import configs
+from urllib.parse import urlencode
 
 CURRENT_WEATHER_BASE_URL = "https://api.weatherapi.com/v1"
 
-# Set up a shared session with default params
-session = requests.Session()
-session.params = {"key": configs.weatherapi_key}  # add default API key
 
 def fetch_weather_from_api(location: str) -> dict:
     logger.info(f"Fetching weather data for {location} from WEATHER API")
 
-    response = session.get(
-        f"{CURRENT_WEATHER_BASE_URL}/current.json",
-        params={"q": location}  # only need to pass the location
-    )
+    encoded_params = urlencode({"key": configs.weatherapi_key, "q": location})
+    url = f"{CURRENT_WEATHER_BASE_URL}/current.json?{encoded_params}"
+    response = requests.get(url)
 
     response.raise_for_status()
     return response.json()
